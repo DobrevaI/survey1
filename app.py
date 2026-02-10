9cdfa683d98756ba79ebb04704c855a88ec2b0e0
DobrevaI <iidobreva24a@gmail.com>
2026-02-10 14:29:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import streamlit as st
 st.title("Survey no theme")
 st.text("I don't really know what this survey will be soooooo")
-planet=st.text_input("Which planet has the most moons?)
+planet=st.text_input("Which planet has the most moons?")
   if st.button "check":
      if planet=saturn or planet=Saturn:
         st.success("That is correct :D")
      else:
         st.error("That would be incorect")
-              
+ocean=st.text_input("Which sea creature has three hearts?")
+  if st.button "check":
+     if ocean=octopus:
+        st.success("That is correct :D")
+     else:
+        st.error("That would be incorect")
+
